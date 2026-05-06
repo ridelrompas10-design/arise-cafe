e54bb64fa523bf0689ded6649d791d8838df8320
rompas13 <ridelrompas09@gmail.com>
2026-05-07 06:55:58 +0800

    fixed rating

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -1,31 +1,14 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Rating extends Model
+public function customer()
 {
-    /**
-     * fillable
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'customer_id',
-        'product_id',
-        'transaction_detail_id',
-        'rating',
-        'review',
-    ];
+    return $this->belongsTo(Customer::class);
+}
 
-    /**
-     * customer
-     *
-     * @return void
-     */
-    public function customer()
-    {
-        return $this->belongsTo(Customer::class);
-    }
+public function product()
+{
+    return $this->belongsTo(Product::class);
+}
+
+public function transactionDetail()
+{
+    return $this->belongsTo(TransactionDetail::class);
 }
