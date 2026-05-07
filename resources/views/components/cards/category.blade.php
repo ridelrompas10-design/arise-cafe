46723e3bcffbcdef21c06eccbacacb1f86de2128
rompas13 <ridelrompas09@gmail.com>
2026-05-07 14:26:23 +0800

    css & categoty

--- resources/views/components/cards/category.blade.php
+++ resources/views/components/cards/category.blade.php
@@ -1,4 +1,4 @@
-<div class="col-4 col-md-3 col-lg-2 mb-3">
+<div class="col-4 col-md-3 mb-3">
     <a href="/category/{{ $category->slug }}" wire:navigate class="text-decoration-none">
         <div class="category-card">
             <div class="category-icon-wrap">
