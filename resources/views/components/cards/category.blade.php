595689e8808f9a40eb5fda3f0ce2311168844689
rompas13 <ridelrompas09@gmail.com>
2026-05-07 15:16:05 +0800

    css & categoty

--- resources/views/components/cards/category.blade.php
+++ resources/views/components/cards/category.blade.php
@@ -1,9 +1,11 @@
 <div class="col">
     <a href="/category/{{ $category->slug }}" wire:navigate class="text-decoration-none">
-        <div class="card border-0 rounded shadow-sm">
-            <div class="card-body text-center">
-                <img src="{{ asset('/storage/' . $category->image) }}" class="img-fluid" width="50">
-                <label class="text-center">{{ $category->name }}</label>
+        <div class="card border-0 rounded-4 shadow-sm h-100 text-center py-3 px-2">
+            <img src="{{ asset('/storage/' . $category->image) }}" 
+                 class="mx-auto" 
+                 style="width: 60px; height: 60px; object-fit: contain;">
+            <div class="card-body p-0 mt-2">
+                <small class="text-dark fw-semibold">{{ $category->name }}</small>
             </div>
         </div>
     </a>
